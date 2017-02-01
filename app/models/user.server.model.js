51e80da3a26034064d1fda01ee0b1692b52cbc15
Pavel Razuvalov <pavel@razuvalov.com>
2017-02-01 23:33:53 +0300

    Local user authentication added

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose'),
+    crypto   = require('crypto'),
     Schema   = mongoose.Schema;
 
 var UserSchema = new Schema({
   role: {
     type: String,
-    enum: ['Admin', 'Owner', 'User']
+    enum: ['Admin', 'Owner', 'User'],
+    default: 'User'
   },
   firstName: String,
   lastName: String,
@@ -24,14 +26,14 @@ var UserSchema = new Schema({
   email: {
     type: String,
     index: true,
-    required: true,
-    match: /.+\@.+\..+/
+    required: 'E-mail is required',
+    match: [/.+\@.+\..+/, "Please fill a valid e-mail address"]
   },
   username: {
     type: String,
     trim: true,
     unique: true,
-    required: true,
+    required: 'Username is required',
     validate: [
       function(username) {
         return username.length > 3;
@@ -49,6 +51,15 @@ var UserSchema = new Schema({
       'Password should be longer'
     ]
   },
+  salt: {
+    type: String
+  },
+  provider: {
+    type: String,
+    required: 'Provider is required'
+  },
+  providerId: String,
+  providerData: {},
   created: {
     type: Date,
     default: Date.now
@@ -65,14 +76,46 @@ UserSchema.virtual('fullName')
     this.lastName = splitName[1] || '';
   });
 
-UserSchema.set('toJSON', { getters: true });
-
-UserSchema.post('save', function(next) {
-  if(this.isNew) {
-    console.log('A new user was created.');
-  } else {
-    console.log('A user updated is details.');
+UserSchema.pre('save', function(next) {
+  if (this.password) {
+    this.salt = new
+    Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+    this.password = this.hashPassword(this.password);
   }
+  next();
+});
+
+UserSchema.methods.hashPassword = function(password) {
+  return crypto.pbkdf2Sync(password, this.salt, 10000,
+  64).toString('base64');
+};
+
+UserSchema.methods.authenticate = function(password) {
+  return this.password === this.hashPassword(password);
+};
+
+UserSchema.statics.findUniqueUsername = function(username, suffix, callback) {
+  var _this = this;
+  var possibleUsername = username + (suffix || '');
+
+  _this.findOne({
+    username: possibleUsername
+  }, function(err, user) {
+    if (!err) {
+      if (!user) {
+        callback(possibleUsername);
+      } else {
+        return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+      }
+    } else {
+      callback(null);
+    }
+  });
+};
+
+UserSchema.set('toJSON', {
+  getters: true,
+  virtuals: true
 });
 
 mongoose.model('User', UserSchema);
--- config/express.js
+++ config/express.js
@@ -4,7 +4,9 @@ var config          = require('./config'),
     compress        = require('compression'),
     bodyParser      = require('body-parser'),
     methodOverride  = require('method-override'),
-    session         = require('express-session');
+    session         = require('express-session'),
+    flash           = require('connect-flash'),
+    passport        = require('passport');
 
 module.exports = function() {
   var app = express();
@@ -14,10 +16,12 @@ module.exports = function() {
   } else if (process.env.NODE_ENV === 'production') {
     app.use(compress());
   }
+
   app.use(bodyParser.urlencoded({
     extended: true
   }));
   app.use(bodyParser.json());
+
   app.use(methodOverride());
 
   app.use(session({
@@ -28,6 +32,10 @@ module.exports = function() {
 
   app.set('views', './app/views');
   app.set('view engine', 'ejs');
+
+  app.use(flash());
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   require('../app/routes/index.server.routes.js')(app);
   require('../app/routes/users.server.routes.js')(app);
